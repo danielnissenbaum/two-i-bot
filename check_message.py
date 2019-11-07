09308c77b1c5a7545655005c7b51fda4c3736ff3
Daniel Nissenbaum <danielnissenbaum@CITA4998-MAC-L.local>
2019-11-07 16:50:33 +0000

    Update check_message.py

--- check_message.py
+++ check_message.py
@@ -4,5 +4,8 @@ def check(payload):
 
     message = payload["event"]["text"]
     if bool(re.search('(?:docs.google.com', payload["event"]["text"])):
-        send(payload)
+        #send(payload)
+        print("GOOGLE DOCCCCCCCAAAAAAA")
+    else:
+        print("IT IS NO GOOGLE")
 return
