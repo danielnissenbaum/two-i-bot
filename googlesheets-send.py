28cd3b81cadd333b308212a924e6db49e969c154
Ian <ian.ansell.uk@gmail.com>
2019-11-07 16:34:47 +0000

    google sheets authentication

--- googlesheets-send.py
+++ googlesheets-send.py
@@ -1,55 +1,28 @@
-from __future__ import print_function
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+import os
+from apiclient import discovery
+from google.oauth2 import service_account
 
-# If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+secret_file = os.path.join("creds/","two-i.json")
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1_2vJ8d2x5fpifx-D7sdmt6IReM9m_f-J1bpCiAt_TaE'
+SPREADSHEET_ID = '1_2vJ8d2x5fpifx-D7sdmt6IReM9m_f-J1bpCiAt_TaE'
 SAMPLE_RANGE_NAME = 'Data!A1:E'
 
-def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+def send(data):
+    print(data)
 
-    service = build('sheets', 'v4', credentials=creds)
-
-    # Call the Sheets API
+    credentials = service_account.Credentials.from_service_account_file(secret_file, scopes=SCOPES)
+    service = discovery.build('sheets','v4', credentials=credentials)
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
+
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,range=SAMPLE_RANGE_NAME).execute()
+
     values = result.get('values', [])
 
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
+    
+
+
 
 if __name__ == '__main__':
-    main()
+    send("you did what?")
