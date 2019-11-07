4df77e2ce1e0aa435d79aea726223c5f60bf5c2f
Daniel Nissenbaum <danielnissenbaum@CITA4998-MAC-L.local>
2019-11-07 15:55:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def challenge():
     print(challenge)
     url = ""
     headers = {"Content-type":"application/json;charset=utf-8", "Authorization":"Bearer "+ str(BOT_USER_TOKEN)}
-    r = requests.post("https://slack.com/api/events", headers=headers, data=json.dumps(challenge))
+    r = requests.post("https://slack.com/api/events", headers=headers, data=challenge)
 
     return make_response("", 200)
 
