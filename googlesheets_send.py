d269e5631acf677f01a1f9242f139da3d5e2115a
Daniel Nissenbaum <danielnissenbaum@CITA4998-MAC-L.local>
2020-02-07 11:26:25 +0000

    Update googlesheets_send.py

--- googlesheets_send.py
+++ googlesheets_send.py
@@ -5,7 +5,7 @@ from google.oauth2 import service_account
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 BASE_PATH = os.path.dirname('two-i-bot')
-secret_file = os.path.join(BASE_PATH,"/creds/","twoi.json")
+secret_file = os.path.join(BASE_PATH,"creds/","twoi.json")
 
 # The ID and range of a sample spreadsheet.
 SPREADSHEET_ID = '1_2vJ8d2x5fpifx-D7sdmt6IReM9m_f-J1bpCiAt_TaE'
